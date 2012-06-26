b9344bdbfd84a1da4457af3806b3243e7a386252
Alexandre Pereira da Silva <aletes.xgr@gmail.com>
2012-06-26 11:27:12 -0300

    usb: gadget: at91_udc: Propagate devicetree to gadget drivers
    commit 65c84ea18b1b4b8c03fb67c3bea023ed1446bd2e upstream.
    
    Fill dev.of_node of gadget drivers, so they can use devicetree
    
    Signed-off-by: Alexandre Pereira da Silva <aletes.xgr@gmail.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/at91_udc.c
+++ drivers/usb/gadget/at91_udc.c
@@ -1650,6 +1650,7 @@ static int at91_start(struct usb_gadget_driver *driver,
 
 	udc->driver = driver;
 	udc->gadget.dev.driver = &driver->driver;
+	udc->gadget.dev.of_node = udc->pdev->dev.of_node;
 	dev_set_drvdata(&udc->gadget.dev, &driver->driver);
 	udc->enabled = 1;
 	udc->selfpowered = 1;
