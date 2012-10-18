50b0e20ba5539ed707bfe144be6abb3d8f4dde61
Dinh Nguyen <dinguyen@altera.com>
2012-10-18 13:39:04 -0600

    FogBugz #80434: Bring all peripherals out of reset
    Write a 0 to the PERMODRST register in the HPS reset manager. This
    releases all of the HPS peripherals from reset.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    
    Conflicts:
    
    	arch/arm/mach-socfpga/core.h

---
+++ arch/arm/mach-socfpga/core.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2012 Pavel Machek <pavel@denx.de>
+ * Copyright (C) 2012 Altera Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __MACH_CORE_H
+#define __MACH_CORE_H
+
+#define SOCFPGA_MODPERRST	0x14
+
+extern void secondary_startup(void);
+extern void __iomem *socfpga_scu_base_addr;
+extern void __iomem *sys_manager_base_addr;
+extern void __iomem *rst_manager_base_addr;
+
+extern void socfpga_secondary_startup(void);
+extern void socfpga_cpu_die(unsigned int cpu);
+extern void socfpga_init_clocks(void);
+
+extern struct dw_mci_board sdmmc_platform_data;
+
+#endif /* __CORE_H */
