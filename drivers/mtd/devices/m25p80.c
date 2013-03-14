ff70064ab106bd387623611748e9b1583dd75963
Lisa Treweek <ltreweek@altera.com>
2013-03-14 18:38:32 -0500

    FogBugz #101898: device node tree corrected for correct flash part
    Added the correct flash part number to the device tree.
    
    Signed-off-by: Lisa Treweek <ltreweek@altera.com>
    
    Conflicts:
    
    	drivers/mtd/devices/m25p80.c

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -709,6 +709,12 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "mx25l25635e", INFO(0xc22019, 0, 64 * 1024, 512, 0) },
 	{ "mx25l25655e", INFO(0xc22619, 0, 64 * 1024, 512, 0) },
 
+	/* Micron */
+	{ "n25q128a11",  INFO(0x20bb18, 0, 64 * 1024, 256, 0) },
+	{ "n25q128a13",  INFO(0x20ba18, 0, 64 * 1024, 256, 0) },
+	{ "n25q256a", INFO(0x20ba19, 0, 64 * 1024, 512, SECT_4K) },
+	{ "n25q00",   INFO(0x20ba21, 0, 64 * 1024, 256, SECT_4K) },
+
 	/* Spansion -- single (large) sector size only, at least
 	 * for the chips listed here (without boot sectors).
 	 */
