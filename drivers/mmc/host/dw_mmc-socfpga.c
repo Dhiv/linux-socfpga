28f6dbfb968568c2d5342adf5eb77fed4356b5fb
Dinh Nguyen <dinguyen@altera.com>
2013-05-22 10:27:08 -0500

    FogBugz #125555: Upgrade to v3.9
    Upgrade the SoCFPGA Linux support to v3.9. Alignment with kernel.org's support
    for SoCFPGA.
    
    - status = "disabled" goes into dtsi files
    - use the syscon driver for dw_mmc-socfpga.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- drivers/mmc/host/dw_mmc-socfpga.c
+++ drivers/mmc/host/dw_mmc-socfpga.c
@@ -29,19 +29,17 @@
 #define SYSMGR_SDMMC_CTRL_SET(smplsel, drvsel)		\
 	((((drvsel) << 0) & 0x7) | (((smplsel) << 3) & 0x38))
 
-extern void __iomem *sys_manager_base_addr;
-
 /* SOCFPGA implementation specific driver private data */
 struct dw_mci_socfpga_priv_data {
 	u8	ciu_div;
 	u32	hs_timing;
+	struct regmap   *sysreg;
 };
 
 static int dw_mci_socfpga_priv_init(struct dw_mci *host)
 {
 	struct dw_mci_socfpga_priv_data *priv;
 	struct device *dev = host->dev;
-	int pwr_en;
 
 	priv = devm_kzalloc(host->dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv) {
